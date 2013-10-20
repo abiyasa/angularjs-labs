e3613caa48e29430ef78126b876711508b9e2094
abiyasa <ilham.suhardi@gmail.com>
2013-10-20 22:41:34 +0200

    Controller to fetch feed data

--- feed/app/scripts/controllers/main.js
+++ feed/app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('readerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'AngularJS',
-      'Karma',
-      'Bootstrap'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D"http%3A%2F%2Fdailyjs.com%2Fatom.xml"%20and%20itemPath%3D"feed.entry"&format=json&diagnostics=true&callback=JSON_CALLBACK';
+
+    // fetch feed
+    $http.jsonp(url)
+      .success(function (data) {
+        console.log(data);
+
+        // feed the scope with the result
+        $scope.feed = {
+          title: 'DailyJS',
+          items: data.query.results.entry
+        };
+      })
+      .error(function (data, status) {
+        // TODO error handling
+        console.log('error on fetching data:', data, status);
+      });
   });
