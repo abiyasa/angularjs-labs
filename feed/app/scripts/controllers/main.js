214bc5736556dee44a683dd00bc71b646b45af6e
abiyasa <ilham.suhardi@gmail.com>
2013-10-22 22:33:06 +0200

    Break the long URL into multiple lines

--- feed/app/scripts/controllers/main.js
+++ feed/app/scripts/controllers/main.js
@@ -2,7 +2,9 @@
 
 angular.module('readerApp')
   .controller('MainCtrl', function ($scope, $http) {
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D"http%3A%2F%2Fdailyjs.com%2Fatom.xml"%20and%20itemPath%3D"feed.entry"&format=json&diagnostics=true&callback=JSON_CALLBACK';
+    var url = 'http://query.yahooapis.com/v1/public/yql?' +
+      'q=select%20*%20from%20xml%20where%20url%3D"http%3A%2F%2Fdailyjs.com%2Fatom.xml"%20and' +
+      '%20itemPath%3D"feed.entry"&format=json&diagnostics=true&callback=JSON_CALLBACK';
 
     // fetch feed
     $http.jsonp(url)
